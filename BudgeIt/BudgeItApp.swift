d367bf79436e92f99d8aadf4d11b370cb7b317d0
Colleen Handel <handelc22@gmail.com>
2024-07-05 15:06:57 -0500

    Initial Commit

---
+++ BudgeIt/BudgeItApp.swift
@@ -0,0 +1,32 @@
+//
+//  BudgeItApp.swift
+//  BudgeIt
+//
+//  Created by Colleen Handel on 7/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BudgeItApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
